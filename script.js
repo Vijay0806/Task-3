e0b9c7ad4a6d6a03c5672458058f78ff4cfc3387
Vijay0806 <vijaym08062001@gmail.com>
2023-03-16 15:13:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let myResume={
+    "basics": {
+      "name": VIJAY M ",
+      "email": "vijaym08062001@gamil.com",
+      "phone": 7603960068,
+      "degree": "B.E",
+      "location": {
+        "address": "1/272D Mallikuttai",
+        "postalCode": 635205,
+        "city": "Dharmapuri",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Vijay0806"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "Government College Of Engineering",
+        "department": "Electronics and Communication Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "cgpa": 7.94,
+      }
+    ],
+    "skills": [
+      {
+        "name": "c,javascript",
+        "level": "beginner",
+        "project": [
+          "self charging electri vechicle using matlab"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": " writter,travelling,gaming,",
+      }
+    ]
+  }
+  console.log(myResume);
